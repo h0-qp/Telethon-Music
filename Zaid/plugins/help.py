c196fc1a3a1ac03c4f4d8c474fe1dbc4b5a782e9
dyler2 <113936853+dyler2@users.noreply.github.com>
2022-12-06 21:54:32 +0300

    Update help.py

--- Zaid/plugins/help.py
+++ Zaid/plugins/help.py
@@ -7,7 +7,7 @@ btn =[
     [Button.inline("Admin", data="admin"), Button.inline("Play", data="play")],
     [Button.inline("Home", data="start")]]
 
-HELP_TEXT = "Welcome To help Menu Section\n\nClick on the Buttons!"
+HELP_TEXT = "اهلا بك في قائمة الاوامر\n\nاختر من الازرار!"
 
 
 @Zaid.on(events.NewMessage(pattern="[!?/]help"))
@@ -15,8 +15,8 @@ async def help(event):
     if Config.MANAGEMENT_MODE == "ENABLE":
         return
     if event.is_group:
-       await event.reply("Contact me in PM to get available help menu!", buttons=[
-       [Button.url("Help And Commands!", "t.me/{}?start=help".format(BOT_USERNAME))]])
+       await event.reply("ارسل لي /help في الخاص لعرض قائمة الاوامر!", buttons=[
+       [Button.url("ارسال /help!", "t.me/{}?start=help".format(BOT_USERNAME))]])
        return
 
     await event.reply(HELP_TEXT, buttons=btn)

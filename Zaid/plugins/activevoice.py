ab95fee96f4618b490229ba8962704ae8dd5967e
dyler2 <113936853+dyler2@users.noreply.github.com>
2022-12-06 22:16:34 +0300

    Update activevoice.py

--- Zaid/plugins/activevoice.py
+++ Zaid/plugins/activevoice.py
@@ -8,7 +8,7 @@ from Zaid.helpers.queues import get_active_chats
 @Zaid.on(events.NewMessage(pattern="^/activevoice"))
 async def activevc(message):
     mystic = await message.reply(
-        "Getting active voice chats.. Please hold"
+        "جارِ الحصول على المكالمات... الرجاء الانتظار"
     )
     served_chats = await get_active_chats()
     text = ""
@@ -25,8 +25,8 @@ async def activevc(message):
             text += f"{j + 1}. {title} [`{x}`]\n"
         j += 1
     if not text:
-        await mystic.edit("No Active Voice Chats")
+        await mystic.edit("ليس هناك محادثة جارية")
     else:
         await mystic.edit(
-            f"**Active Voice Chats:-**\n\n{text}"
+            f"**وجدت محادثة:-**\n\n{text}"
         )
